4b41cd9fc2649dd2a0598f7ead949f63359d3865
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2025-12-02 06:26:21 +0000

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,4 @@
+def display_message(username):
+    """display a simple message to everyone"""
+    print(f"Hey {username}, i am learning python functions in this chapter")
+display_message('kofi')
---
+++ example2.py
@@ -0,0 +1,4 @@
+def favorite_book(title):
+    """display a message of the book i like best"""
+    print(f"my favorite book is {title.title()}")
+favorite_book('abdul in Dubai')
